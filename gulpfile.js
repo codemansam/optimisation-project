71139e9b05ecda0bf13d94655ed1d599b54019bd
Sam <sam@stacked.co.nz>
2017-01-23 09:47:17 +1300

    Used gulp to compress style.css and inlined it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp'),
-    minifyHtml = require("gulp-minify-html");
+    minifyHtml = require("gulp-minify-html"),
+      cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-html', function () {
     gulp.src('pizza.html')
     .pipe(minifyHtml())
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('cleanCSS', function() {
+  return gulp.src('dev/css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('default',['minify-html'],function(){
