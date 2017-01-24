bfdf84614a5c52f546ed470a319037a9c963a778
Sam <sam@stacked.co.nz>
2017-01-24 14:47:19 +1300

    Solved pernicious css tidying problems from gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     minifyHtml = require("gulp-minify-html"),
-      cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    concatCSS = require('gulp-concat-css');
 
 gulp.task('minify-html', function () {
     gulp.src('pizza.html')
@@ -9,9 +10,15 @@ gulp.task('minify-html', function () {
 });
 
 gulp.task('cleanCSS', function() {
-  return gulp.src('dev/css/bootstrap-grid.css')
+  return gulp.src('dev/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('concatCSS', function () {
+  return gulp.src('dist/css/*.css')
+    .pipe(concatCSS('final.css'))
+    .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('default',['minify-html'],function(){
